f1bc9907421f1b848d82f1572ac83f57aa5bda8a
Dmitry Kutsenko <d2emonium@gmail.com>
2023-12-02 14:29:05 +0300

    Convert App to functional component

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+    Container,
+    Row,
+    Col, Card,
+} from 'reactstrap';
+// Components
+import Header from '../../components/Header';
+// Containers
+import Aber from '../Aber/Aber';
+import MainStarter from '../Main/Starter';
+import Starter from '../Starter';
+import Compass from '../Compass';
+import Room from '../Room';
+// Styles
+import './index.css';
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Container className="App-intro">
+      <Card>
+        <Aber />
+      </Card>
+      <Row>
+        <Col><MainStarter /></Col>
+      </Row>
+      <Row>
+        <Col xs="2"><Starter /></Col>
+        <Col><Room /></Col>
+      </Row>
+      <Row>
+        <Col><Compass /></Col>
+      </Row>
+    </Container>
+  </div>
+);
+
+export default App;
