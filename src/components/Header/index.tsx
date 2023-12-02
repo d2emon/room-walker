986ee1fae619a29c7348657a24291b72d79279ee
Dmitry Kutsenko <d2emonium@gmail.com>
2023-12-02 14:32:18 +0300

    Convert Header to typescript

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="App-header mb-2">
+    <h1 className="App-title">Room Walker</h1>
+  </header>
+);
+
+export default Header;
--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import {
     Container,
     Row,
     Col, Card,
 } from 'reactstrap';
 // Components
-import Header from '../../components/Header';
+import Header from 'components/Header';
 // Containers
 import Aber from '../Aber/Aber';
 import MainStarter from '../Main/Starter';
