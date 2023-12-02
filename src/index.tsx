78cf28bda45cf5c9218a6b6d8728fb601c19fed4
Dmitry Kutsenko <d2emonium@gmail.com>
2023-12-02 14:25:28 +0300

    Move redux initialization to store

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// TODO: Move redux to store module
-import {
-    createStore,
-    applyMiddleware,
-    combineReducers,
-} from 'redux';
 import { Provider } from 'react-redux';
-// TODO: Move redux to store module
-import thunk from 'redux-thunk';
 import App from './containers/App';
 // TODO: Change registerServiceWorker to reportWebVitals
 import registerServiceWorker from './registerServiceWorker';
-// TODO: Move redux to store module
-import * as reducers from './store/reducers';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-
-const store = createStore(
-    combineReducers(reducers),
-    applyMiddleware(thunk),
-);
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
@@ -34,4 +20,5 @@ root.render(
     </React.StrictMode>
 );
 
+// TODO: Change registerServiceWorker to reportWebVitals
 registerServiceWorker();
