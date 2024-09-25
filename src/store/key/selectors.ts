f20ad7e073211ffdad7560d5e09dcc7be4f09254
Dmitry Kutsenko <d2emonium@gmail.com>
2024-09-25 11:58:19 +0300

    Fix errors

--- src/store/key/selectors.ts
+++ src/store/key/selectors.ts
@@ -1,8 +1,8 @@
 import { Store } from 'store';
 import { KeyFlags } from './interface';
-import { getPrDue } from '../aber2/external';
+// import { getPrDue } from '../aber2/external';
 
 export const getSaved = (store: Store): KeyFlags => store.keys.saved;
-export const getNeedReprint = (store: Store): boolean => !store.keys.isInput && getPrDue();
+export const getNeedReprint = (store: Store): boolean => !store.keys.isInput; // && getPrDue();
 export const getReprint = (store: Store): string => `\n${store.keys.prompt}${store.keys.keyBuffer}`;
 
